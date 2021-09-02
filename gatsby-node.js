fcec16fe9ee2f6fca822b06a20d455d41811e07b
gdhardy1 <gdhardy1@gmail.com>
2021-09-02 14:40:06 -0400

    add case 11 and 12

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,13 +3,23 @@ const { redirects } = require("./gatsby/cloudRedirects")
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage, createRedirect } = actions
-
+  const toPathTemplate = path.resolve("./src/templates/to-path.js")
   redirects.forEach(r => {
     createRedirect(r)
     createPage({
       path: r.toPath,
-      component: path.resolve("./src/templates/to-path.js"),
+      component: toPathTemplate,
       context: r,
     })
+  })
+
+  createPage({
+    path: "/case-11",
+    component: toPathTemplate,
+  })
+
+  createPage({
+    path: "/case-12/",
+    component: toPathTemplate,
   })
 }
