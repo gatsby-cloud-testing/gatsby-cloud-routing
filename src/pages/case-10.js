11d466865a536daef35d1125364f9098315bfdf6
gdhardy1 <gdhardy1@gmail.com>
2021-09-02 14:45:11 -0400

    add message to case 9 and 10

--- src/pages/case-10.js
+++ src/pages/case-10.js
@@ -1,7 +1,12 @@
 import * as React from "react"
 
 const PageExists = () => {
-  return <h1>Case 10</h1>
+  return (
+    <h1>
+      This file was created from a file named case-10.js placed in the src/pages
+      directory
+    </h1>
+  )
 }
 
 export default PageExists
--- src/pages/case-9/index.js
+++ src/pages/case-9/index.js
@@ -1,7 +1,12 @@
 import * as React from "react"
 
 const PageExists = () => {
-  return <h1>Case 9</h1>
+  return (
+    <h1>
+      This file was created from a file named index.js placed in the
+      src/pages/case-9 directory. directory
+    </h1>
+  )
 }
 
 export default PageExists
