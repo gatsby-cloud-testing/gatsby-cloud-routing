b856c53b145cc1888c30f56978fb7d9d088185f5
gdhardy1 <gdhardy1@gmail.com>
2021-09-08 08:17:24 -0400

    stringify req.body

--- src/api/body.js
+++ src/api/body.js
@@ -2,7 +2,9 @@ import { Buffer } from "buffer"
 
 const handler = async (req, res) => {
   try {
-    res.status(200).json({ bytes: Buffer.byteLength(req.body, "utf-8") })
+    res
+      .status(200)
+      .json({ bytes: Buffer.byteLength(JSON.stringify(req.body), "utf-8") })
   } catch (error) {
     console.log(error)
     res.status(500).json({ error })
