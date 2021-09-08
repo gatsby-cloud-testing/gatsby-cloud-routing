50ee411fcd211be39d934ce1646d85e92903ec52
gdhardy1 <gdhardy1@gmail.com>
2021-09-08 08:12:41 -0400

    handle errors

--- src/api/body.js
+++ src/api/body.js
@@ -1,7 +1,12 @@
 import { Buffer } from "buffer"
 
 const handler = async (req, res) => {
-  res.status(200).json({ bytes: Buffer.byteLength(req.body, "utf-8") })
+  try {
+    res.status(200).json({ bytes: Buffer.byteLength(req.body, "utf-8") })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error })
+  }
 }
 
 export default handler
