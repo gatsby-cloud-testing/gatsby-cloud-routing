35afb855b7f3fd0942202bf39f66da74d20f7e5d
gdhardy1 <gdhardy1@gmail.com>
2021-09-08 08:19:20 -0400

    add body to response

--- src/api/body.js
+++ src/api/body.js
@@ -1,10 +1,11 @@
 import { Buffer } from "buffer"
 
 const handler = async (req, res) => {
+  let body = JSON.stringify(req.body)
   try {
     res
       .status(200)
-      .json({ bytes: Buffer.byteLength(JSON.stringify(req.body), "utf-8") })
+      .json({ bytes: Buffer.byteLength(body, "utf-8"), string: body })
   } catch (error) {
     console.log(error)
     res.status(500).json({ error })
