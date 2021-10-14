2349f887229b9964345f4e3a23a7c820c1b7aeb1
gdhardy1 <gdhardy1@gmail.com>
2021-10-14 14:39:19 -0400

    show query params in function

--- src/api/body.js
+++ src/api/body.js
@@ -2,10 +2,11 @@ import { Buffer } from "buffer"
 
 const handler = async (req, res) => {
   let body = JSON.stringify(req.body)
+  let query = JSON.stringify(req.query)
   try {
     res
       .status(200)
-      .json({ bytes: Buffer.byteLength(body, "utf-8"), string: body })
+      .json({ bytes: Buffer.byteLength(body, "utf-8"), body, query })
   } catch (error) {
     console.log(error)
     res.status(500).json({ error })
